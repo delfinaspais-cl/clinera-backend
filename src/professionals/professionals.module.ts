6439af013715108574c558fdc74015a44ffbb924
delfinaspais-cl <delfina.spais@oacg.cl>
2025-09-03 16:26:23 -0300

    saco jwt en tratamientos

--- src/professionals/professionals.module.ts
+++ src/professionals/professionals.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProfessionalsService } from './professionals.service';
 import { ProfessionalsController } from './professionals.controller';
 import { SucursalesController } from './sucursales.controller';
-import { TratamientosController } from './tratamientos.controller';
+// import { TratamientosController } from './tratamientos.controller'; // Removido - usando el nuevo módulo
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [ProfessionalsController, SucursalesController, TratamientosController],
+  controllers: [ProfessionalsController, SucursalesController], // TratamientosController removido
   providers: [ProfessionalsService],
   exports: [ProfessionalsService], // Exportar el servicio para que otros módulos puedan usarlo
 })
