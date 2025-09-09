c6f8884a949ea8ca4680b71fb2c1cf9e11964bd0
delfinaspais-cl <delfina.spais@oacg.cl>
2025-09-08 23:38:17 -0300

    arreglo endpoint user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,37 @@
+import { IsString, IsEmail, IsEnum, IsOptional, IsArray, MinLength } from 'class-validator';
 
+export enum UserRole {
+  ADMINISTRADOR = 'ADMIN',
+  SECRETARIO = 'SECRETARY', 
+  PROFESIONAL = 'PROFESSIONAL'
+}
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
+  nombre: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsEnum(UserRole)
+  tipo: UserRole;
+
+  @IsOptional()
+  @IsString()
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  sucursales?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  especialidades?: string[];
+}
