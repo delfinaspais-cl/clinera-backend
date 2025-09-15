a1017ebcfe9bac20377a786a892a0c8e94ee25f2
delfinaspais-cl <delfina.spais@oacg.cl>
2025-09-15 10:58:01 -0300

    corrijo deploy para endpoints publicos

--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from '../prisma/prisma.module';
   imports: [PrismaModule],
   controllers: [PatientsController],
   providers: [PatientsService],
+  exports: [PatientsService],
 })
 export class PatientsModule {}
